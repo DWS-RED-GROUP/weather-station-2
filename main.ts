591a1a55df0c04e8cc7b0a9398e270d217b0a745
DWS-RED-GROUP <138417142+DWS-RED-GROUP@users.noreply.github.com>
2023-07-11 11:55:02 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+serial.onDataReceived(serial.delimiters(Delimiters.CarriageReturn), function () {
+    rawData = serial.readUntil(serial.delimiters(Delimiters.CarriageReturn))
+    windDirection = rawData.substr(7, 3)
+    windSpeed = rawData.substr(11, 4)
+    temp = rawData.substr(57, 3)
+    humidity = rawData.substr(61, 2)
+    airPressure = rawData.substr(65, 5)
+    rain = rawData.substr(31, 4)
+    bluetooth.uartWriteLine("" + windDirection + "," + windSpeed + "," + convertToText(parseFloat(temp) / 10) + "," + humidity + "," + airPressure + "," + rain)
+})
+let rain = ""
+let airPressure = ""
+let humidity = ""
+let temp = ""
+let windSpeed = ""
+let windDirection = ""
+let rawData = ""
+serial.redirect(
+SerialPin.P13,
+SerialPin.P14,
+BaudRate.BaudRate9600
+)
+bluetooth.startUartService()
+serial.setRxBufferSize(74)
+basic.forever(function () {
+	
+})
